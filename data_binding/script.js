84e43fbc72055340616af93ebe6f3b07a572a0f2
danielpartida <daniel.partida@d-fine.de>
2022-05-01 15:25:49 +0200

    Create and display matrix

--- data_binding/script.js
+++ data_binding/script.js
@@ -1,17 +1,25 @@
-var myData = [4, 1, 6, 2, 8, 9]
-     
-var p = d3.select("body")
-    .selectAll("span")
-    .data(myData)
-    .enter()
-    .append('span')
-    .style('color', function(d) {
-        if (d % 2 === 0) {
-            return "green";
-        } else {
-            return "red";
-        }
+var matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+];
+
+var tr = d3.select("body")
+            .append("table")  // adds <table>
+            .selectAll("tr")  // selects all <tr>
+            .data(matrix)      // joins matrix array 
+            .enter()           // create placeholders for each row in the array
+            .append("tr");
+
+var td = tr.selectAll("td")
+    .data(function (d) {    // joins inner array of each row
+        console.log(d);
+        return d;
     })
-    .text(function (d, i) {
-        return d + " ";
+    .enter()    // create placeholders for each element in an inner array
+    .append("td") // creates <td> in each placeholder
+    .text(function (d) {
+        console.log(d);
+        return d; // add value of each inner array as a text in <td>
     });
