1a5b17f4eb20cdc22145bb3c59b0aa93dbd5808e
danielpartida <daniel.partida@d-fine.de>
2022-05-01 15:49:22 +0200

    Debugging json data file

--- data_loading/script.js
+++ data_loading/script.js
@@ -1,3 +1,14 @@
-d3.csv("employee.csv", function(data) {
-    console.log(data);
+// d3.csv("data/employee.csv", function(data) {
+//     console.log(data);
+// });
+
+d3.json("data/users.json", function(error, data) {
+
+    console.log("Testing");
+
+    if (error) {
+        return console.warn(error);
+    }
+
+    console.log("Printing data:", data);
 });
