7bc78cf2879de9305d66fbcee66988f53599bc56
danielpartida <daniel.partida@d-fine.de>
2022-05-01 16:58:56 +0200

    Fix json load
    https://stackoverflow.com/questions/49768165/code-within-d3-json-callback-is-not-executed

--- data_loading/script.js
+++ data_loading/script.js
@@ -1,14 +1,24 @@
-// d3.csv("data/employee.csv", function(data) {
-//     console.log(data);
-// });
+d3.csv("data/employee.csv", function(data) {
+    console.log("CSV data", data);
+});
 
-d3.json("data/users.json", function(error, data) {
 
-    console.log("Testing");
+// https://stackoverflow.com/questions/49768165/code-within-d3-json-callback-is-not-executed
+d3.json("data/geeks.json").then(function(data, error) {
+
+    console.log("JSON data", data);
 
     if (error) {
         return console.warn(error);
     }
 
-    console.log("Printing data:", data);
+    d3.select("body")
+        .selectAll("p")
+        .data(data)
+        .enter()
+        .append("p")
+        .text(function(d) {
+            return d.name + ", " + d.location;
+        });
+
 });
