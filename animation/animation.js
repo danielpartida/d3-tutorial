4863c9d29384679e578ed47dc6c66aab2cd90a92
danielpartida <daniel.partida@d-fine.de>
2022-05-01 15:07:35 +0200

    Create bar animation

--- animation/animation.js
+++ animation/animation.js
@@ -1,6 +1,34 @@
-var t = d3.transition()
-        .duration(3000)
+var svg = d3.select("body")
+        .append("svg")
+        .attr("width", 1000)
+        .attr("height", 1000);
 
-d3.select("#container")
-          .transition(t)
-          .style("background-color", "red");
+
+    var bar1 = svg.append("rect")
+        .attr("fill", "blue")
+        .attr("x", 100)
+        .attr("y", 20)
+        .attr("height", 20)
+        .attr("width", 10)
+
+    var bar2 = svg.append("rect")
+        .attr("fill", "blue")
+        .attr("x", 120)
+        .attr("y", 20)
+        .attr("height", 20)
+        .attr("width", 10)
+
+    update();
+
+function update() {
+        bar1.transition()
+            .ease(d3.easeLinear)
+            .duration(2000)
+            .attr("height",120)
+    
+        bar2.transition()
+            .ease(d3.easeLinear)
+            .duration(2000)
+            .delay(2000)
+            .attr("height",100)
+    }
